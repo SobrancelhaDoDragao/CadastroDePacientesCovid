478adb6c514d5fd5ab217703dd9c1a7df5d14a8e
Eudson Durães <eudson.duraes@gmail.com>
2020-07-30 22:16:13 -0300

    Adicionando boostrap nos formulários

--- ManterPacientes/forms.py
+++ ManterPacientes/forms.py
@@ -4,6 +4,10 @@ from .models import Paciente
 
 class PacienteForm(forms.ModelForm):
 
+    NomeCompleto = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'placeholder': 'Digite seu nome completo','class':'form-control'}))
+    DataNascimento = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'type':'date','class':'form-control'}))
+    
     class Meta:
         model = Paciente
         fields = ('NomeCompleto','DataNascimento', "TipoTeste","ResultadoTeste")
+       
